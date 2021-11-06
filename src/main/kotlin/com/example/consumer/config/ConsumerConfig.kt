4e697d0ce1f3bab99c01ff9a19ec3f69af147d78
Esterfano Lopes <esterfano.lopes@gmail.com>
2021-11-06 16:22:10 -0300

    first commit

---
+++ src/main/kotlin/com/example/consumer/config/ConsumerConfig.kt
@@ -0,0 +1,31 @@
+package br.com.devcave.rabbit.config
+
+import br.com.devcave.rabbit.service.BasicListener
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class ConsumerConfig(
+    private val connectionFactory: ConnectionFactory,
+    private val basicListener: BasicListener,
+    private val simpleRabbitListenerContainerFactory: SimpleRabbitListenerContainerFactory
+) {
+
+    @Bean
+    fun listenerContainer(): MessageListenerContainer {
+        val container = SimpleMessageListenerContainer()
+        container.connectionFactory = connectionFactory
+        container.setQueueNames("SECOND-QUEUE-BASIC")
+        container.setMessageListener(basicListener)
+        simpleRabbitListenerContainerFactory.adviceChain?.let {
+            container.setAdviceChain(*it)
+        }
+
+        return container
+    }
+}
